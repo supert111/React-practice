9e497e519e6e6775b9481559757b16c86c31a332
supert111 <sportsmen@bigmir.net>
2021-04-15 16:48:31 +0300

    link

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+
+const link = React.createElement('a', { href: "reactjs.org" }, "Ссылочка" );
+
+console.log(link);
