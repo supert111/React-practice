aa566bc8a7e9ee387f45fa153f30b11e5750bc20
supert111 <sportsmen@bigmir.net>
2021-04-30 01:34:40 +0300

    Updates

--- static/js/main.3761c068.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-practice"]=this["webpackJsonpreact-practice"]||[]).push([[0],{9:function(e,c,t){"use strict";t.r(c);var r=t(1),n=t.n(r),a=t(3),s=t.n(a),i=t(0);var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"Hello React Mykola "})})};s.a.render(Object(i.jsx)(n.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.3761c068.chunk.js.map
